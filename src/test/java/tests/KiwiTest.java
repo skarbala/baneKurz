4bfee39d9f614c8feb11f07179ad7e0104754fdb
skarbala <skarbala.martin@gmail.com>
2020-03-24 14:47:55 +0100

    video 05. Trable s lokatormi, vytahujeme cenu letenky

--- src/test/java/tests/KiwiTest.java
+++ src/test/java/tests/KiwiTest.java
@@ -3,8 +3,6 @@ package tests;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Cookie;
 
@@ -20,13 +18,13 @@ public class KiwiTest {
         WebDriverRunner.getWebDriver().manage().addCookie(cookie);
         refresh();
         $(byAttribute("data-test", "LandingSearchButton")).click();
-        System.out.println("");
         //1.kliknut na prvy vyskyt/ponuku destinacie
         $(byAttribute("data-test", "PictureCard")).click();
         //2.pockam kym sa mi nacitaju letenky
-        $(byAttribute("data-test", "ResultCardWrapper"))
+        SelenideElement wrapper = $(byAttribute("data-test", "ResultCardWrapper"))
                 .waitUntil(Condition.appears, 15000);
         //3.ulozim si cenu letenky do premennej
+        String price = wrapper.find("strong[class*='PriceText']").shouldNotBe(Condition.empty).getText();
         //4.otvorim detail letenky a porovnam cenu
     }
 }
