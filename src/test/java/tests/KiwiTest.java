72f0a619fd2812aa13ff1b5a8bae068976475e96
skarbala <skarbala.martin@gmail.com>
2020-03-25 13:11:42 +0100

    video 07. Java local variable

--- src/test/java/tests/KiwiTest.java
+++ src/test/java/tests/KiwiTest.java
@@ -1,7 +1,6 @@
 package tests;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Cookie;
@@ -15,17 +14,17 @@ public class KiwiTest {
     @Test
     void itShouldOpenMainPage() {
         open("https://www.kiwi.com/en");
-        Cookie cookie = new Cookie("cookie_consent", "agreed");
+        var cookie = new Cookie("cookie_consent", "agreed");
         WebDriverRunner.getWebDriver().manage().addCookie(cookie);
         refresh();
         $(byAttribute("data-test", "LandingSearchButton")).click();
         //1.kliknut na prvy vyskyt/ponuku destinacie
         $(byAttribute("data-test", "PictureCard")).click();
         //2.pockam kym sa mi nacitaju letenky
-        SelenideElement wrapper = $(byAttribute("data-test", "ResultCardWrapper"))
+        var wrapper = $(byAttribute("data-test", "ResultCardWrapper"))
                 .waitUntil(Condition.appears, 15000);
         //3.ulozim si cenu letenky do premennej
-        String price = wrapper.find("strong[class*='PriceText']").shouldNotBe(Condition.empty).getText();
+        var price = wrapper.find("strong[class*='PriceText']").shouldNotBe(Condition.empty).getText();
         //4.otvorim detail letenky a porovnam cenu
         wrapper.click();
         $("div[data-test='ModalFooter'] div[class*=FooterPriceWrapper]")
@@ -33,10 +32,5 @@ public class KiwiTest {
         $("div[data-test='ModalFooter']").find(byText("Book"))
                 .shouldBe(Condition.enabled)
                 .shouldBe(Condition.visible);
-    }
-
-
-    public static void main(String[] args) {
-
     }
 }
