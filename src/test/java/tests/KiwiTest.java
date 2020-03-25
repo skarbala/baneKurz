7f9795c558754eefd1c5e8eabd423d2aa7a76922
skarbala <skarbala.martin@gmail.com>
2020-03-25 21:12:50 +0100

    video 11. bleskova refaktorizacia

--- src/test/java/tests/KiwiTest.java
+++ src/test/java/tests/KiwiTest.java
@@ -1,9 +1,13 @@
 package tests;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
@@ -13,17 +17,22 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class KiwiTest {
 
+    @BeforeAll
+    static void config() {
+        Configuration.baseUrl = "https://www.kiwi.com/en";
+        Configuration.startMaximized = true;
+    }
+
     @Test
     void itShouldOpenMainPage() {
-        open("https://www.kiwi.com/en");
-        var cookie = new Cookie("cookie_consent", "agreed");
-        WebDriverRunner.getWebDriver().manage().addCookie(cookie);
+        open("/");
+        setCookieConsent();
         refresh();
-        $(byAttribute("data-test", "LandingSearchButton")).click();
+        $(byDataTestAttribute("LandingSearchButton")).click();
         //1.kliknut na prvy vyskyt/ponuku destinacie
-        $(byAttribute("data-test", "PictureCard")).click();
+        $(byDataTestAttribute("PictureCard")).click();
         //2.pockam kym sa mi nacitaju letenky
-        var wrapper = $(byAttribute("data-test", "ResultCardWrapper"))
+        var wrapper = $(byDataTestAttribute("ResultCardWrapper"))
                 .waitUntil(Condition.appears, 15000);
         //3.ulozim si cenu letenky do premennej
         var price = wrapper.find("strong[class*='PriceText']").shouldNotBe(Condition.empty).getText();
@@ -35,24 +44,22 @@ public class KiwiTest {
                 .shouldBe(Condition.enabled)
                 .shouldBe(Condition.visible);
     }
-    @Test
+
+    @RepeatedTest(10)
     void itShouldFindNomadTripAndCheckPriceOnReservation() {
-        open("https://www.kiwi.com/en/nomad");
-        var cookie = new Cookie("cookie_consent", "agreed");
-        WebDriverRunner.getWebDriver().manage().addCookie(cookie);
+        open("/nomad");
+        setCookieConsent();
         refresh();
-        //1.klikni na tlacidlo show popular journeys
-        $(byText("Show popular journeys")).click();
-        //2.vyber cestu po azii
+        //1.vyber cestu po azii
         $$("div[class*=ExampleItemstyled]")
                 .shouldHave(sizeGreaterThan(1))
                 .find(Condition.text("Asia"))
                 .click();
-        //3.pockaj kym sa zobrazi booking button a klikni nan
-        $(byAttribute("data-test", "BookingButton"))
+        //2.pockaj kym sa zobrazi booking button a klikni nan
+        $(byDataTestAttribute("BookingButton"))
                 .waitUntil(Condition.appears, 15000)
                 .click();
-        //4.over ze cena na registracii nie je prazdna
+        //3.over ze cena na registracii nie je prazdna
         $("div.ReservationBill-item-price")
                 .waitUntil(Condition.appears, 15000)
                 .shouldNotBe(Condition.empty);
@@ -60,26 +67,25 @@ public class KiwiTest {
 
     @Test
     void itShouldChangeCurrencyAndCheckPrice() {
-        open("https://www.kiwi.com/en/");
-        var cookie = new Cookie("cookie_consent", "agreed");
-        WebDriverRunner.getWebDriver().manage().addCookie(cookie);
+        open("/");
+        setCookieConsent();
         refresh();
         //1.zmen menu a vyber si japonsky yen
-        $(byAttribute("data-test", "Currency-Open")).click();
-        $(byAttribute("data-test", "Currency-Item-jpy")).click();
-        $(byAttribute("data-test", "Currency")).shouldHave(Condition.text("JPY"));
+        $(byDataTestAttribute("Currency-Open")).click();
+        $(byDataTestAttribute("Currency-Item-jpy")).click();
+        $(byDataTestAttribute("Currency")).shouldHave(Condition.text("JPY"));
         //2.ako cielovu destinaciu vyber tokyo
         //2a. zadaj do druheho inputu tokyo
         //2b. zaklikni prvy checkbox
-        $$(byAttribute("data-test", "SearchField-input"))
+        $$(byDataTestAttribute("SearchField-input"))
                 .get(1)
                 .val("Tokyo");
         $("div[data-test='PlacePickerInput-destination'] input").val("Tokyo");
-        $(byAttribute("data-test", "PlacePickerRowCheckbox")).click();
+        $(byDataTestAttribute("PlacePickerRowCheckbox")).click();
         //3.klikni na search button
         $(byText("Search")).click();
         //4.pockaj kym sa nacitaju letenky (vyuzi metodu z prveho testu)
-        var wrapper = $(byAttribute("data-test", "ResultCardWrapper"))
+        var wrapper = $(byDataTestAttribute("ResultCardWrapper"))
                 .waitUntil(Condition.appears, 15000);
         //5.over ze cena obsahuje menu japonsky yen
         wrapper.find("strong[class*='PriceText']").shouldHave(Condition.text("¥"));
@@ -88,5 +94,14 @@ public class KiwiTest {
     @AfterEach
     void quitDriver() {
         WebDriverRunner.getWebDriver().quit();
+    }
+
+    private By byDataTestAttribute(String landingSearchButton) {
+        return byAttribute("data-test", landingSearchButton);
+    }
+
+    private void setCookieConsent() {
+        var cookie = new Cookie("cookie_consent", "agreed");
+        WebDriverRunner.getWebDriver().manage().addCookie(cookie);
     }
 }
