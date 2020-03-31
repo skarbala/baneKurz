6dbc964959c85fdde5c44495a7a2edcec1561568
skarbala <skarbala.martin@gmail.com>
2020-03-31 18:09:07 +0200

    video 25. spajame test watcher a BS API

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -7,10 +7,16 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.SessionId;
 
 import static utils.Utils.generateBuildName;
 
+@ExtendWith(Watcher.class)
 public class TestBase {
+
+    private static SessionId sessionId;
 
     @BeforeAll
     static void config() {
@@ -26,8 +32,13 @@ public class TestBase {
         BrowserBase.getCapabilities().setCapability("name", testName);
     }
 
+    public static SessionId getSessionId() {
+        return sessionId;
+    }
+
     @AfterEach
     void quitDriver() {
+        sessionId = ((RemoteWebDriver) WebDriverRunner.getWebDriver()).getSessionId();
         WebDriverRunner.getWebDriver().quit();
     }
 }
--- src/test/java/base/Watcher.java
+++ src/test/java/base/Watcher.java
@@ -2,15 +2,19 @@ package base;
 
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestWatcher;
+import utils.BrowserStackIntegration;
 
 public class Watcher implements TestWatcher {
     @Override
     public void testSuccessful(ExtensionContext context) {
-        System.out.println("test passed");
+        var sessionId = TestBase.getSessionId().toString();
+        BrowserStackIntegration.setResult(sessionId, "passed", "OK");
     }
 
     @Override
     public void testFailed(ExtensionContext context, Throwable cause) {
-        System.out.println("ou I failed");
+        var sessionId = TestBase.getSessionId().toString();
+        BrowserStackIntegration.setResult(sessionId, "failed", "NOK");
     }
+
 }
