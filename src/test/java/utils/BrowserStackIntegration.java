ba1e8599227259a25d4b11b0ee07489fd1b794b2
skarbala <skarbala.martin@gmail.com>
2020-03-31 23:31:50 +0200

    video 28. public_url

--- src/test/java/utils/BrowserStackIntegration.java
+++ src/test/java/utils/BrowserStackIntegration.java
@@ -13,4 +13,20 @@ public class BrowserStackIntegration {
                 .body(result)
                 .asJson();
     }
+
+    public static String getPublicUrl(String sessionId) {
+        return Unirest.get("https://api.browserstack.com/automate/sessions/{sessionId}.json")
+                .routeParam("sessionId", sessionId)
+                .basicAuth("matko5", "wba3Hey1qW54Lt5Hz67U")
+                .asJson().getBody()
+                .getObject()
+                .getJSONObject("automation_session")
+                .get("public_url")
+                .toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getPublicUrl("8b4ee4972f4e0edd11e32cc2b91919cdd257f394"));
+
+    }
 }
