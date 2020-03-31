2900be022f73b2e6f777ffff713b7822f38e1708
skarbala <skarbala.martin@gmail.com>
2020-03-31 13:00:09 +0200

    video 23. nastavujeme vysledok testu

--- src/test/java/utils/BrowserStackIntegration.java
+++ src/test/java/utils/BrowserStackIntegration.java
@@ -5,10 +5,19 @@ import kong.unirest.Unirest;
 public class BrowserStackIntegration {
 
     public static void main(String[] args) {
-        Unirest.put("https://api.browserstack.com/automate/sessions/b4275aba42e11ee982e052c7be4882351823e1b4.json")
+        setResult(
+                "b4275aba42e11ee982e052c7be4882351823e1b4",
+                "passed",
+                "som opat stastna ze brano je triezvy"
+        );
+    }
+
+    public static void setResult(String sessionId, String status, String reason) {
+        Unirest.put("https://api.browserstack.com/automate/sessions/{sessionId}.json")
+                .routeParam("sessionId", sessionId)
                 .header("Content-Type", "application/json")
                 .basicAuth("matko5", "wba3Hey1qW54Lt5Hz67U")
-                .body("{\"name\":\"nora\"}")
+                .body("{\"status\":\"" + status + "\", \"reason\":\"" + reason + "\"}")
                 .asJson();
     }
 }
